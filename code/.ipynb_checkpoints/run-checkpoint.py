14db520df8e29fcae1c93626ffb83d77a0b0df1f
simonasnow <simona.frenda@gmail.com>
2024-03-08 20:36:03 +0100

    release of code

--- code/.ipynb_checkpoints/run-checkpoint.py
+++
@@ -1,6 +0,0 @@
-from config import config
-from perspectivist_experiments import datasets, training_models, performance
-
-# datasets.create_dataset(config)
-training_models.get_predictions(config)
-# performance.get_results(config)
